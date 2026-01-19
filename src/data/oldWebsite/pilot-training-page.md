86ee26615aaa0533192df753af1b47db037693ee
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2026-01-19 17:26:04 -0300

    Add pilot training info old website

---
+++ src/data/oldWebsite/pilot-training-page.md
@@ -0,0 +1,122 @@
+CONTACT US
+Pitcairn Flight Academy LLC logo
+(610) 495-7000
+Pitcairn Flight Academy Website Designed and Developed by Right Rudder Marketing
+Pitcairn Logo
+FLY WITH US
+BECOME A PILOT
+ABOUT US
+DISCOVERY FLIGHT
+CONTACT US
+The Pilot Training Process
+How to become a pilot
+Pitcairn Flight Academy is the perfect place to kickstart your pilot training journey. We're here to help you get your wings and achieve your dream of becoming a pilot. Join us and start on an exciting adventure in the world of aviation.
+
+✈
+Our Instructors are Professional, Safe, and Friendly
+✈
+When You Want to Fly, We're There
+✈
+Learning Tailored to You in Our Private Classrooms
+✈
+A Supportive Community for You and Your Family
+ENROLL TODAY
+pilots in front of plane
+student pilot and cfi at pitcairn flight school
+Pilot Training Pathway
+The first thing we recommend you to do is to schedule a discovery flight. This is where you take the controls of an airplane and go flying with a flight instructor. After the flight, you can decide if this is right for you.
+
+If you want to continue, then we'll pair you up with an instructor and you'll start training. From there you'll get the aeronautical knowledge needed to pass your pilot exams. You start off with obtaining your private pilot certificate which allows you to fly planes with passengers. From there, you can pursue additional ratings and certifications depending on what your aviation goals are.
+
+Are you up for the challenge? Try taking our fun quiz. Discover what your learning styles are and see if flight training is right for you.
+
+TAKE THE QUIZ
+Cessna landing during a discovery flight. Flickr Jason Woodhead
+Why do we recommend a discovery flight?
+A discovery flight is a sneak peek into the world of aviation and a chance to get your feet a little wet. We think it's a pretty good idea and the best first step for anyone dreaming of flying. At Pitcairn Flight Academy, we're all about providing a hands on experience and making sure you're making the right call when it comes to your aviation journey.
+
+During a discovery flight, you get to hop into the cockpit, take the controls, and feel what it's like to be the one flying the plane. It's a bit like a test drive for flying, without the pressure of committing to a full blown pilot's license. We recommend it because there's usually two outcomes out of the introductory flight. One, either you don't like it and you can determine that becoming is not right for you. Or two, you fall in love with it and you make the decision that becoming a pilot is right for you. It's usually the latter. So don't wait, get on the schedule today and let's go flying together.
+
+LET'S GO FLYING
+Start with a discovery flight
+Also known as an intro flight, this is the first flight you'll take where you're the pilot.
+
+SCHEDULE TODAY
+I liked the discovery flight and I want to be a pilot.
+What's next?
+This really depends on your aviation goals. Are you looking to make a career out of flying or do you want to fly for fun? Many pilots stop at their private or instrument and fly around town leisurely, while other pilots who want to make a career out of flying move on to getting their commercial certificate and flight instructor certificates. Contact us and talk to one of our team members to determine what's right for you.
+
+CONTACT US
+Private Pilot Training at Pitcairn Flight Academy
+Private Pilot
+This is the first step for folks who dream of flying. This certification lets you fly a plane and take passengers. Our skilled instructors are all about giving you the right training so you can ace the basics of flying. They'll make sure you can take off, land, and cruise the skies with confidence. Our team's got your back!
+
+PRIVATE PILOT TRAINING
+Instrument Training at Pitcairn Flight Academy
+Instrument Rating
+An Instrument Rating is your ticket to mastering flying in bad weather and low visibility, using only your plane's instruments. It's all about gaining the ability to rely solely on the instruments within your aircraft, enabling you to make safe and informed decisions. It opens up a bunch of new opportunities, making you a safer and more skilled pilot.
+
+INSTRUMENT RATING
+Commercial Training at Pitcairn Flight Academy
+Commercial Pilot
+Obtain a commercial certificate is where you get a start in your aviation career. A commercial certificate lets you get paid for your flying skills. Our experienced instructors will teach you how to handle more complex planes, learn more advanced maneuvers, and handle emergencies. We'll make sure you're ready to tackle any challenges in your aviation career.
+
+COMMERCIAL CERTIFICATE
+Certified Flight Instructor (CFI) at Pitcairn Flight Academy
+Flight Instructor
+Teach new pilots, gain flight experience, and share your knowledge. When you become a Certified Flight Instructor (CFI) at Pitcairn Flight Academy, you're guaranteed an interview to teach here. Get your flight hours with us, and when you're ready, you can move on to work for the airlines or continue instructing and help teach the next generation of aviators.
+
+CERTIFIED FLIGHT INSTRUCTOR
+Still looking for more information on pilot training?
+
+Get Your FREE Pilot Training Guide Book!
+We've prepared this free booklet which explains everything you need to know for your pilot certificate.
+
+DOWNLOAD THE FREE BOOK
+Picture of our free how to be a pilot training guide
+Contact Us
+Fill out the form below and a member of our team will reach out to you shortly
+Name
+Email
+If you are a human, ignore this field
+Phone
+###-###-####
+How can we help?
+logo backdrop
+Our Commitment
+Our unwavering commitment is to train student pilots to be successful in their careers. Our passion is aviation and we're invested in helping you accomplish your aviation goals. Give us a call today and let's fly.
+
+New to Flying?
+Your First Flight
+
+How to Become a Pilot
+
+Why Become a Pilot
+
+Aircraft Rentals
+
+PITCAIRN FLIGHT
+About Us
+
+Why Pitcairn
+
+Blog
+
+Contact Us
+
+Contact
+3310 W Ridge Pike
+Pottstown, PA 19464
+
+flynow@pitcairnflight.com
+
+(610) 495-7000
+
+© 2026 Pitcairn Flight Academy LLC. All Rights Reserved.
+Privacy Policy & Terms of Use
+Facebook page
+Google Page
+Pitcairn Flight Academy Website Designed and Developed by Right Rudder Marketing
+
+Avatar
+Hi there, have a question? Text us here.
---
+++ src/data/oldWebsite/pilot-training.js
@@ -0,0 +1,142 @@
+const pilotTrainingContent = {
+  hero: {
+    title: "The Pilot Training Process",
+    subtitle: "How to become a pilot",
+    description:
+      "Pitcairn Flight Academy is the perfect place to kickstart your pilot training journey. We're here to help you get your wings and achieve your dream of becoming a pilot. Join us and start on an exciting adventure in the world of aviation.",
+    bullets: [
+      "Our Instructors are Professional, Safe, and Friendly",
+      "When You Want to Fly, We're There",
+      "Learning Tailored to You in Our Private Classrooms",
+      "A Supportive Community for You and Your Family",
+    ],
+    cta: {
+      text: "ENROLL TODAY",
+      link: "/enroll-at-pitcairn-flight-academy",
+    },
+    images: [
+      {
+        alt: "pilots in front of plane",
+        src: "/images/pilots-front-plane.jpg",
+      },
+      {
+        alt: "student pilot and cfi at pitcairn flight school",
+        src: "/images/student-cfi-training.jpg",
+      },
+    ],
+  },
+
+  pathway: {
+    title: "Pilot Training Pathway",
+    description:
+      "The first thing we recommend you to do is to schedule a discovery flight. This is where you take the controls of an airplane and go flying with a flight instructor. After the flight, you can decide if this is right for you.\n\nIf you want to continue, then we'll pair you up with an instructor and you'll start training. From there you'll get the aeronautical knowledge needed to pass your pilot exams. You start off with obtaining your private pilot certificate which allows you to fly planes with passengers. From there, you can pursue additional ratings and certifications depending on what your aviation goals are.",
+    cta: {
+      text: "TAKE THE QUIZ",
+      link: "/quiz",
+      subtext:
+        "Are you up for the challenge? Try taking our fun quiz. Discover what your learning styles are and see if flight training is right for you.",
+    },
+    image: {
+      alt: "Cessna landing during a discovery flight",
+      src: "/images/cessna-landing-discovery.jpg",
+      credit: "Flickr Jason Woodhead",
+    },
+  },
+
+  discoveryFlight: {
+    title: "Why do we recommend a discovery flight?",
+    description:
+      "A discovery flight is a sneak peek into the world of aviation and a chance to get your feet a little wet. We think it's a pretty good idea and the best first step for anyone dreaming of flying. At Pitcairn Flight Academy, we're all about providing a hands on experience and making sure you're making the right call when it comes to your aviation journey.\n\nDuring a discovery flight, you get to hop into the cockpit, take the controls, and feel what it's like to be the one flying the plane. It's a bit like a test drive for flying, without the pressure of committing to a full blown pilot's license. We recommend it because there's usually two outcomes out of the introductory flight. One, either you don't like it and you can determine that becoming is not right for you. Or two, you fall in love with it and you make the decision that becoming a pilot is right for you. It's usually the latter. So don't wait, get on the schedule today and let's go flying together.",
+    cta: {
+      text: "LET'S GO FLYING",
+      link: "/discovery-flight",
+    },
+    steps: [
+      {
+        title: "Start with a discovery flight",
+        description:
+          "Also known as an intro flight, this is the first flight you'll take where you're the pilot.",
+        cta: {
+          text: "SCHEDULE TODAY",
+          link: "/discovery-flight",
+        },
+      },
+      {
+        title:
+          "I liked the discovery flight and I want to be a pilot. What's next?",
+        description:
+          "This really depends on your aviation goals. Are you looking to make a career out of flying or do you want to fly for fun? Many pilots stop at their private or instrument and fly around town leisurely, while other pilots who want to make a career out of flying move on to getting their commercial certificate and flight instructor certificates.",
+        cta: {
+          text: "CONTACT US",
+          link: "/contact-us",
+          subtext:
+            "Contact us and talk to one of our team members to determine what's right for you.",
+        },
+      },
+    ],
+  },
+
+  trainingPrograms: {
+    title: "Training Programs",
+    programs: [
+      {
+        title: "Private Pilot",
+        description:
+          "This is the first step for folks who dream of flying. This certification lets you fly a plane and take passengers. Our skilled instructors are all about giving you the right training so you can ace the basics of flying. They'll make sure you can take off, land, and cruise the skies with confidence. Our team's got your back!",
+        cta: {
+          text: "PRIVATE PILOT TRAINING",
+          link: "/private-pilot-training",
+        },
+        imageAlt: "Private Pilot Training at Pitcairn Flight Academy",
+      },
+      {
+        title: "Instrument Rating",
+        description:
+          "An Instrument Rating is your ticket to mastering flying in bad weather and low visibility, using only your plane's instruments. It's all about gaining the ability to rely solely on the instruments within your aircraft, enabling you to make safe and informed decisions. It opens up a bunch of new opportunities, making you a safer and more skilled pilot.",
+        cta: {
+          text: "INSTRUMENT RATING",
+          link: "/instrument-rating",
+        },
+        imageAlt: "Instrument Training at Pitcairn Flight Academy",
+      },
+      {
+        title: "Commercial Pilot",
+        description:
+          "Obtain a commercial certificate is where you get a start in your aviation career. A commercial certificate lets you get paid for your flying skills. Our experienced instructors will teach you how to handle more complex planes, learn more advanced maneuvers, and handle emergencies. We'll make sure you're ready to tackle any challenges in your aviation career.",
+        cta: {
+          text: "COMMERCIAL CERTIFICATE",
+          link: "/commercial-pilot-training",
+        },
+        imageAlt: "Commercial Training at Pitcairn Flight Academy",
+      },
+      {
+        title: "Flight Instructor",
+        description:
+          "Teach new pilots, gain flight experience, and share your knowledge. When you become a Certified Flight Instructor (CFI) at Pitcairn Flight Academy, you're guaranteed an interview to teach here. Get your flight hours with us, and when you're ready, you can move on to work for the airlines or continue instructing and help teach the next generation of aviators.",
+        cta: {
+          text: "CERTIFIED FLIGHT INSTRUCTOR",
+          link: "/become-a-cfi",
+        },
+        imageAlt:
+          "Certified Flight Instructor (CFI) at Pitcairn Flight Academy",
+      },
+    ],
+  },
+
+  resources: {
+    title: "Still looking for more information on pilot training?",
+    offer: {
+      title: "Get Your FREE Pilot Training Guide Book!",
+      description:
+        "We've prepared this free booklet which explains everything you need to know for your pilot certificate.",
+      cta: {
+        text: "DOWNLOAD THE FREE BOOK",
+        link: "/free-guide",
+      },
+      image: {
+        alt: "Picture of our free how to be a pilot training guide",
+        src: "/images/free-pilot-training-guide.jpg",
+      },
+    },
+  },
+};
