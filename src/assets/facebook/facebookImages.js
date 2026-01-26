2407e126151d4003039b7b546863979742c5e1b2
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2026-01-26 13:51:56 -0300

    Update images names

---
+++ src/assets/facebook/facebookImages.js
@@ -0,0 +1,54 @@
+const images = [
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_both-planes-side-by-side-back.jpg",
+    alt: "Both planes side by side at Pitcairn Flight Academy in Pottstown, PA pilot training",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_first-solo-student-red-plane.jpg",
+    alt: "First solo student with red plane at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_first-solo-woman-student-red-plane.jpg",
+    alt: "First solo woman student with red plane at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_flying-plane-over-country-side.jpg",
+    alt: "Flying plane over countryside at Pitcairn Flight Academy in Pottstown, PA pilot training",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_john-gunson-passing-private-pilot-checkride.jpg",
+    alt: "John Gunson passing private pilot checkride at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_pilot-smilling-to-camera-inside-cockpit.jpg",
+    alt: "Pilot smiling to camera inside cockpit at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_pitcairn-flight-academy-junior-aviators-camp.jpg",
+    alt: "Pitcairn Flight Academy Junior Aviators Camp in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_plane-flying-country-side.jpg",
+    alt: "Plane flying over countryside at Pitcairn Flight Academy in Pottstown, PA pilot training",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_plane-vision-clouds-sunset.jpg",
+    alt: "Plane vision with clouds at sunset at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_planes-on-front-of-hangar.jpg",
+    alt: "Planes in front of hangar at Pitcairn Flight Academy in Pottstown, PA pilot training",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_red-and-white-plane-cessna-hangar.jpg",
+    alt: "Red and white Cessna plane in hangar at Pitcairn Flight Academy in Pottstown, PA",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_two-planes-flying-side-by-side.jpg",
+    alt: "Two planes flying side by side at Pitcairn Flight Academy in Pottstown, PA pilot training",
+  },
+  {
+    src: "src/assets/facebook/pitcairn-flight-academy_pottstown-PA_pilot-training_vision-plane-good-points-of-reference.jpg",
+    alt: "Vision plane with good points of reference at Pitcairn Flight Academy in Pottstown, PA",
+  },
+];
